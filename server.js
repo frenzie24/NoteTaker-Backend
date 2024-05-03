d9dead766dd23049707300f3993736451e074441
frenzie24 <kevincharlesgross@gmail.com>
2024-05-03 12:26:51 -0400

    added logic to fetch data from db.json on GET /api/notes added filestream to server.js added some comments

--- server.js
+++ server.js
@@ -1,27 +1,46 @@
 const express = require('express');
+const fs = require('fs');
 const path = require('path');
-
+// var to store data from db
+const notesData = require('./db/db.json')
 const app = express();
 const PORT = 3001;
 
-// TODO: Invoke app.use() and serve static files from the '/public' folder
+// Serving files from the public folder
 app.use(express.static('public'));
-app.get('/', (req, res) => res.send('Navigate to /send or /routes'));
+//app.get('/', (req, res) => res.send('Navigate to /send or /routes'));
 
-//
+
+// ## region API 
+/*
+    fetch the notes from the db
+*/
+app.get('/api/notes', (req, res) => {
+    res.json(notesData);
+    console.log('Notes data: \n', notesData);
+});
+
+
+app.post('/api/notes', (req, res) => {
+    res.json(notesData);
+    console.log('Notes data: \n', notesData);
+});
+
+// ## endregion
+
 app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, 'public/notes.html'))
+    res.sendFile(path.join(__dirname, 'public/notes.html'))
 );
 
 //back to home with an invalid path!
 app.get('/*', (req, res) =>
-  res.sendFile(path.join(__dirname, 'public/index.html'))
+    res.sendFile(path.join(__dirname, 'public/index.html'))
 );
 
 // TODO: Create a route that will serve up the `public/paths.html` page
-app.get('/paths', (req, res)=> {
-  res.sendFile(path.join(__dirname, 'public/paths.html'))
+app.get('/paths', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/paths.html'))
 });
 app.listen(PORT, () =>
-  console.log(`Example app listening at http://localhost:${PORT}`)
+    console.log(`Example app listening at http://localhost:${PORT}`)
 );
