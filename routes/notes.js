903ee82a42b5bbe9949e5080d826134c35bd6f92
frenzie24 <kevincharlesgross@gmail.com>
2024-05-03 16:56:23 -0400

    added functionality to notes router to delete notes

--- routes/notes.js
+++ routes/notes.js
@@ -1,5 +1,6 @@
 const fb = require('express').Router();
-const { readAndAppend, readFromFile } = require('../scripts/fsUtils');
+const { readAndAppend, readFromFile, writeToFile } = require('../scripts/fsUtils');
+const { log, info, warn } = new (require('../scripts/logger'));
 const uuid = require('../scripts/uuid');
 
 // GET Route for retrieving all notes
@@ -13,10 +14,10 @@ fb.post('/', (req, res) => {
     const { title, text } = req.body;
 
     // If all the required properties are present
-    if (title && text ) {
+    if (title && text) {
         // Variable for the object we will save
         const newNote = {
-            title, 
+            title,
             text,
             id: uuid(),
         };
@@ -32,6 +33,30 @@ fb.post('/', (req, res) => {
     } else {
         res.json('Error in posting feedback');
     }
+});
+
+fb.delete('/*', (req, res) => {
+    log(req.params[0], 'magenta');
+    let notes = [];
+    readFromFile('./db/db.json').then((data) => {
+        notes = JSON.parse(data);
+        res.json(JSON.parse(data))
+        log(['old notes', notes], 'red', 'bgBlack');
+        notes = notes.filter((note) => note.id != req.params[0])
+
+        log(['new notes', notes], 'green', 'yellow');
+       // fb.close();
+        writeToFile('./db/db.json', notes);
+
+    }
+    ).then(() => {
+        /*writeToFile('./db/db.json', notes).then(res => {
+            log(res);
+            return;
+        })*/
+        log(notes, 'magenta');
+
+    })
 });
 
 module.exports = fb;
